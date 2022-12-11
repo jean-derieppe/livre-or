72a1d8ed0dd194ed1a1c7c0b2dd2af0720afa91f
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-11 15:32:57 +0100

    connect OK

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+//Définir les variables ( pas obligatoire )  
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$bdd = 'livreor';
+
+//On établit la connexion
+$conn = new mysqli('localhost','root', '', 'livreor');
+?>
