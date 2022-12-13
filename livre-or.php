288ad168e63a131a17475c24099334082fab3cf2
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-13 20:41:57 +0100

    en cour

--- livre-or.php
+++ livre-or.php
@@ -1,3 +1,21 @@
+<?php
+   session_start();
+   include("header.php");
+   require_once("Connect.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Livre d'or</title>
+</head>
+<body>
+    <h1></h1>
+
+</body>
+</html>
+
 Sur cette page on voit l’ensemble des commentaires, organisés du plus récent
 au plus ancien. Chaque commentaire doit être composé d’un texte “posté le
 `jour/mois/année` par `utilisateur`” suivi du commentaire. Si l’utilisateur est
